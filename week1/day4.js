e104fc482a81846a1cf4c5f47b618d8d9d6ace7e
melerons <degefommeron@gmail.com>
2019-06-06 07:09:44 +0200

    my second java script

--- week1/day4.js
+++ week1/day4.js
@@ -1,1 +1,20 @@
 
+var book = {
+  title: "lela sew" ,
+  author: "dr. mhret debebe" ,
+  releasedate: "2004 E.c",
+}
+//to give information about the book
+console.log( book )
+//to print out the book title
+console.log( book.title )
+//
+var food = ["salad","meat","derowet"]
+for ( i = 0; i<=food.length; i++){
+  console.log(food[i])
+}
+//to display numbers from 1 to ten
+var count=1;
+while(count<11){
+  console.log(count++)
+}
