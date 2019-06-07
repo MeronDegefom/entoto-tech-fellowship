90b8c713f82b00adcf3f00daf140f53693bc5656
melerons <degefommeron@gmail.com>
2019-06-07 15:32:10 -0700

    homework day4

---
+++ week1/day4exercise.js
@@ -0,0 +1,11 @@
+for ( var i = 1; i < 21; i++)
+  {
+    if( i % 2 == 0)
+      {
+        console.log( i + ",it is an even number" )
+      }
+else
+  {
+    console.log( i +",it is an odd number" )
+  }
+  }
